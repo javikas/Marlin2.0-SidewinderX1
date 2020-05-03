3b296007595a3a6dea75e35797ee835bda236d14
javikas <javikas@gmail.com>
2020-05-04 00:39:11 +0200

    invert dir

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -121,7 +121,7 @@
  *
  * :[2400, 9600, 19200, 38400, 57600, 115200, 250000, 500000, 1000000]
  */
-#define BAUDRATE 250000
+#define BAUDRATE 115200
 
 // Enable the Bluetooth serial interface on AT90USB devices
 //#define BLUETOOTH
@@ -1061,9 +1061,9 @@
 // @section machine
 
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
-#define INVERT_X_DIR false
-#define INVERT_Y_DIR false
-#define INVERT_Z_DIR true
+#define INVERT_X_DIR true // false TMC2206 inverts DIR
+#define INVERT_Y_DIR true // false TMC2206 inverts DIR
+#define INVERT_Z_DIR false // true TMC2206 inverts DIR (BMG extruder too)
 
 // @section extruder
 
